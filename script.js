b87aaa4349a7ba37a416196cf0b2fcdeda16e4bc
vittorioaugusto <vi.aborges47@gmail.com>
2024-03-05 13:14:22 -0300

    botão de download

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const wrapper = document.querySelector(".wrapper");
 const qrInput = wrapper.querySelector(".form input");
 const generateBtn = wrapper.querySelector(".form button");
 const qrImg = wrapper.querySelector(".qr-code img");
+const downloadBtn = wrapper.querySelector(".download-button"); // Novo
 
 generateBtn.addEventListener("click", () => {
   const qrValue = qrInput.value;
   
   if (!qrValue) {
-    alert("Por favor, insira uma URL ou texto antes de gerar o QR Code.");
+    Swal.fire({
+      title: "Atenção",
+      text: "Por favor, insira uma URL ou texto antes de gerar o QR Code.",
+      icon: "warning",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "OK"
+    });
     return;
-}
+  }
   
   updateButtonState("Gerando um Qr Code...");
 
@@ -18,12 +25,21 @@ generateBtn.addEventListener("click", () => {
   qrImg.addEventListener("load", () => {
     updateButtonState("QR Code Gerado com Sucesso!");
     wrapper.classList.add("active");
+    downloadBtn.classList.remove("hidden");
   });
+});
+
+downloadBtn.addEventListener("click", () => {
+  const downloadLink = document.createElement("a");
+  downloadLink.href = qrImg.src;
+  downloadLink.download = "qrcode.png";
+  downloadLink.click();
 });
 
 qrInput.addEventListener("keyup", () => {
   if (!qrInput.value) {
     resetFormState();
+    downloadBtn.classList.add("hidden");
   }
 });
 
